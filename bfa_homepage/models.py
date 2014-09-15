3ad23373c7bea5d10a09f75f4970805bc2e607e4
Jamie Alexandre <jamalex@gmail.com>
2014-09-14 23:11:05 -0700

    Add model manager and base class for editable items.

--- bfa_homepage/models.py
+++ bfa_homepage/models.py
@@ -1,26 +1,36 @@
 from django.db import models
 
 
-class HomepageSpotlightLink(models.Model):
+class EditableItemManager(models.Manager):
+
+    def published(self):
+        return self.filter(show=True).order_by("sort_order")
+
+
+class EditableItemModel(models.Model):
+    sort_order = models.FloatField(default=0)
+    show = models.BooleanField(default=False)
+
+    objects = EditableItemManager()
+
+    class Meta:
+        abstract = True
+
+
+class HomepageSpotlightLink(EditableItemModel):
     title = models.CharField(max_length=60)
     description = models.TextField()
     image = models.ImageField(upload_to="homepage_spotlight_links")
     url = models.CharField(max_length=100)
-    sort_order = models.FloatField(default=0)
-    show = models.BooleanField(default=False)
 
 
-class HomepageLearnMoreLink(models.Model):
+class HomepageLearnMoreLink(EditableItemModel):
     title = models.CharField(max_length=40)
     description = models.TextField()
     icon_image = models.ImageField(upload_to="homepage_learn_more_links")
     url = models.CharField(max_length=100)
-    sort_order = models.FloatField(default=0)
-    show = models.BooleanField(default=False)
 
 
-class HomepageTestimonial(models.Model):
+class HomepageTestimonial(EditableItemModel):
     text = models.TextField()
     author = models.CharField(max_length=80)
-    sort_order = models.FloatField(default=0)
-    show = models.BooleanField(default=False)
